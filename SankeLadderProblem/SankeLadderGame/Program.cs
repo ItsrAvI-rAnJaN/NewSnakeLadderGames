e8a11babb6a1b10d4a787ea80a7666454a0cb4ca
ravi04ez <ezekie.em@gmail.com>
2022-07-12 17:14:33 +0530

    Welcm Msg Snake&Ladder

---
+++ SankeLadderProblem/SankeLadderGame/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SnakeLadderGame
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t\t\t######################################################");
+            Console.WriteLine("\t\t\t\t\tWELCOME TO SNAKE & LADDER GAME PROGRAM");
+            Console.WriteLine("\t\t\t\t######################################################");
+            Console.ReadKey();
+        }
+    }
+}
