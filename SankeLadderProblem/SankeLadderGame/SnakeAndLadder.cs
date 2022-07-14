815dc911a6c67ea4dda1e91c5b5f14f9a12a4c10
ravi04ez <ezekie.em@gmail.com>
2022-07-14 17:04:24 +0530

    Add Checks for options

--- SankeLadderProblem/SankeLadderGame/SnakeAndLadder.cs
+++ SankeLadderProblem/SankeLadderGame/SnakeAndLadder.cs
@@ -9,7 +9,16 @@ namespace SankeLadderGame
     public class SnakeAndLadder
     {
         int Position = 0;
+        int CurrentPosition;
+
+        // Declaring const Variables to Check options...
+        const int  NoPlay= 0;
+        const int Ladder= 1;
+        const int Snake= 2;
+
         int DiceRolls;
+        int CheckOptions;
+        Random random = new Random();
 
         public  void StartGame()
         {
@@ -17,9 +26,37 @@ namespace SankeLadderGame
         
             Console.WriteLine($"Start  Position of Player = {0}",Position);
 
-            Random random = new Random();
+            // to roll the Dice..
             DiceRolls= random.Next(1,7);
             Console.WriteLine("Player gets the Number After Rolling Dice = {0}",DiceRolls);
+
+            // To Check options..
+            CheckOptions=random.Next(0,3);
+            switch (CheckOptions)
+            {
+                case NoPlay:
+                    Console.WriteLine("Player Options = NoPlay");
+                    CurrentPosition = Position;
+                    break;
+                case Ladder:
+                    Console.WriteLine("Player Options = Ladder");
+                    CurrentPosition += DiceRolls;
+                    break;
+                case Snake:
+                    Console.WriteLine("Player Options = Snake");
+                    
+                    if(CurrentPosition > DiceRolls)
+                    {
+                        CurrentPosition -= DiceRolls;
+                    }
+                    else
+                    {
+                        CurrentPosition = 0;
+                    }
+                    break;
+            }
+            Console.WriteLine("Player Current Position : {0}",CurrentPosition);
+
         }
 
     }
