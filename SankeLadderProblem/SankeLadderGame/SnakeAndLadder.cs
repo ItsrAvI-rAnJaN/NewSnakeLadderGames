37254caf5b8907ac63d8f84510fbf9521bfbdde4
ravi04ez <ezekie.em@gmail.com>
2022-07-14 16:19:35 +0530

    Add DiceRolls

--- SankeLadderProblem/SankeLadderGame/SnakeAndLadder.cs
+++ SankeLadderProblem/SankeLadderGame/SnakeAndLadder.cs
@@ -9,12 +9,17 @@ namespace SankeLadderGame
     public class SnakeAndLadder
     {
         int Position = 0;
+        int DiceRolls;
 
         public  void StartGame()
         {
             Console.WriteLine("The Game Begins....\n");
         
             Console.WriteLine($"Start  Position of Player = {0}",Position);
+
+            Random random = new Random();
+            DiceRolls= random.Next(1,7);
+            Console.WriteLine("Player gets the Number After Rolling Dice = {0}",DiceRolls);
         }
 
     }
